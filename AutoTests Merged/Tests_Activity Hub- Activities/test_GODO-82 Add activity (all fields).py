bcf09be98a3fb7088ff7b1bb494e8a24377c9377
6196511 <39330561+6196511@users.noreply.github.com>
2018-05-17 12:32:35 +0300

    Add files via upload

--- AutoTests Merged/Tests_Activity Hub- Activities/test_GODO-82 Add activity (all fields).py
+++ AutoTests Merged/Tests_Activity Hub- Activities/test_GODO-82 Add activity (all fields).py
@@ -10,7 +10,7 @@ from string import digits
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 import time
-from admin_creds import login, password
+from creds import admin_login, admin_password
 
 class BaseTest(object):
     def teardown_class(self):
@@ -21,8 +21,8 @@ class Test_GODO82(BaseTest):
         get_driver().maximize_window()
         page = loginpage()
         page.open()
-        page.login_field.send_keys(login)
-        page.password_field.send_keys(password)
+        page.login_field.send_keys(admin_login)
+        page.password_field.send_keys(admin_password)
         page.button.click()
         page=ActivityHubPage()
         page.open()
@@ -110,9 +110,45 @@ class Test_GODO82(BaseTest):
         text = page.activity_title.get_attribute("textContent")
         assert text == NewActivityName
         page.edit_activity.click()
-
-        # assert page.is_element_present('activity_actions')
-        # page.activity_actions.click()
-        # wait = WebDriverWait(get_driver(), 15)
-        # wait.until(lambda driver: page.is_element_present('edit_activity'))
-        # page.edit_activity.click()
+        page = AddEditActivityPage()
+        time.sleep(15)
+        assert page.activity_name.get_attribute('value') == NewActivityName
+        assert page.activity_url.get_attribute('value')== NewActivityURL
+        select = Select(page.activity_status)
+        assert select.first_selected_option.text == NewActivityStatus
+        select = Select(page.branch)
+        assert select.first_selected_option.text == NewActivityBranch
+        select = Select(page.starting_location)
+        assert select.first_selected_option.text == NewActivityLocation
+        select = Select(page.time_zone)
+        assert select.first_selected_option.text == NewActivityTimezone
+        assert page.activity_description.get_attribute('value') == NewActivityDesription
+        assert page.cancellation_policy.get_attribute('value') == NewActivityCancellationPolicy
+        assert page.sales_tax.get_attribute('value') == NewActivitySalesTax
+        assert page.activity_duration_days.get_attribute('value') == NewActivityDurationDays
+        assert page.activity_duration_hours.get_attribute('value') == NewActivityDurationHours
+        assert page.activity_duration_minutes.get_attribute('value') == NewActivityDurationMinutes
+        select = Select(page.activity_color)
+        assert select.first_selected_option.text == NewActivityColor
+        assert page.ticket_maximum.get_attribute('value') == NewActivityMaxTickets
+        select = Select(page.sell_out_alert)
+        assert select.first_selected_option.text == NewActivitySellOut
+        select = Select(page.alert_guide_upon_sellout)
+        assert select.first_selected_option.text == NewActivityGuideUponSellout
+        select = Select(page.stop_booking_sold)
+        assert select.first_selected_option.text == NewActivityStopbookingSold+' '
+        assert page.ticket_minimum.get_attribute('value') == NewActivityMinTickets
+        assert page.minimum_not_met_alert.get_attribute('value') == NewActivityNotmetAlert
+        assert page.stop_no_sales.get_attribute('value') == NewActivityStopbookingNoSales
+        assert page.first_ticket_type.get_attribute('value') == NewActivityFirstTicketType
+        assert page.first_ticket_price.get_attribute('value') == NewActivityFirstTicketPrice
+        select = Select(page.first_guide)
+        assert select.first_selected_option.text == NewActivityFirstGuide
+        select = Select(page.first_linked_activity)
+        assert select.first_selected_option.text == NewActivityLinked
+        assert page.what_included.get_attribute('value') == NewActivityWhatIncluded
+        assert page.what_know.get_attribute('value') == NewActivityWhatKnow
+        assert page.what_bring.get_attribute('value') == NewActivityWhatBring
+        select = Select(page.review_redirect)
+        assert select.first_selected_option.text == NewActivityStarsReview
+        assert page.review_website.get_attribute('value') == NewActivityURL
