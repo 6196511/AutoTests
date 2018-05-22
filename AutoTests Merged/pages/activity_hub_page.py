b3e45a7cfba132595ca01a34609c184b28aa4ed6
6196511 <39330561+6196511@users.noreply.github.com>
2018-05-22 12:04:16 +0300

    Add files via upload

--- AutoTests Merged/pages/activity_hub_page.py
+++ AutoTests Merged/pages/activity_hub_page.py
@@ -8,4 +8,5 @@ class ActivityHubPage(BasePage):
     search_activity_field = Find(by=By.XPATH, value="//input[@placeholder='Start typing to search...']")
     activity_title = Find(by=By.XPATH, value="//h2[@class='hub-card-title ng-binding']")
     activity_actions = Find(by=By.XPATH, value="//button[@class='btn btn-primary dropdown-toggle activity-actions-btn']")
-    edit_activity = Find(by=By.XPATH, value="//*[@id='activityBG']/div[3]/div[3]/div/div/div[1]/div/div/div[4]/ul/li[2]/a")
+    edit_activity = Find(by=By.XPATH, value="//i[@class='fa fa-pencil mr20']")
+    show_inactive = Find(by=By.XPATH, value="//*[@id='activityBG']/div[3]/div[2]/div/div/label")
