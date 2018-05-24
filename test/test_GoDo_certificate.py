79b0fa64935435a845b3c779f57a1da95844456b
Viktor <viktor8623@mail.ru>
2018-05-24 21:40:57 +0300

    small refactoring

--- test/test_GoDo_certificate.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from data.add_certificate import testdata
-
-
-@pytest.mark.parametrize("certificate", testdata, ids=[repr(x) for x in testdata])
-def test_purchasing_certificate(app, certificate):
-    """Selling certificates via admin."""
-    app.certificate.add_new_certificate(certificate)
-    app.certificate.verify_created_certificate(certificate)
