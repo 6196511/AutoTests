8734f26509cef4529d3c5b43820e1108112521ae
Viktor Yenjchak <viktor8623@mail.ru>
2018-06-19 23:27:58 +0300

    Locators are fixed.

--- test/test_GoDo_admin_certificate.py
+++ test/test_GoDo_admin_certificate.py
@@ -1,8 +1,8 @@
 import pytest
-from data.certificates import testdata1, testdata2
+from data.orders import admin_certificates
 
 
-@pytest.mark.parametrize("certificate", testdata1, ids=[repr(x) for x in testdata1])
+@pytest.mark.parametrize("certificate", admin_certificates, ids=[repr(x) for x in admin_certificates])
 def test_purchasing_certificate(app, certificate):
     """Selling certificates via admin."""
     app.certificate.select_certificate(certificate)
@@ -10,10 +10,10 @@ def test_purchasing_certificate(app, certificate):
     app.certificate.verify_created_certificate(certificate)
 
 
-@pytest.mark.parametrize("certificate", testdata2, ids=[repr(x) for x in testdata2])
-def test_purchasing_certificate_declines(app, certificate):
-    """Selling certificates via admin."""
-    app.certificate.select_certificate(certificate)
-    app.certificate.make_declined_payment(certificate)
-    app.certificate.make_successful_payment(certificate)
-    app.certificate.verify_created_certificate(certificate)
+# @pytest.mark.parametrize("certificate", testdata2, ids=[repr(x) for x in testdata2])
+# def test_purchasing_certificate_declines(app, certificate):
+#     """Selling certificates via admin."""
+#     app.certificate.select_certificate(certificate)
+#     app.certificate.make_declined_payment(certificate)
+#     app.certificate.make_successful_payment(certificate)
+#     app.certificate.verify_created_certificate(certificate)
