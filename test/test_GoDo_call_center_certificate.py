8af0c734ae17e86c20fbe512d4ac3fda6e1d2c7f
Viktor Yenjchak <viktor8623@mail.ru>
2018-06-29 21:35:53 +0300

    Fixture has been fixed.

--- test/test_GoDo_call_center_certificate.py
+++ test/test_GoDo_call_center_certificate.py
@@ -3,17 +3,17 @@ from data.orders import center_certificates, center_certificates_declines
 
 
 @pytest.mark.parametrize("certificate", center_certificates, ids=[repr(x) for x in center_certificates])
-def test_purchasing_certificate(app, certificate):
+def test_purchasing_certificate(call_center, certificate):
     """Selling certificates via call center."""
-    app.certificate.select_certificate(certificate)
-    app.certificate.make_successful_payment(certificate)
-    app.certificate.verify_created_certificate(certificate)
+    call_center.certificate.select_certificate(certificate)
+    call_center.certificate.make_successful_payment(certificate)
+    call_center.certificate.verify_created_certificate(certificate)
 
 
 @pytest.mark.parametrize("certificate", center_certificates_declines, ids=[repr(x) for x in center_certificates_declines])
-def test_purchasing_certificate_declines(app, certificate):
+def test_purchasing_certificate_declines(call_center, certificate):
     """Selling certificates via call center."""
-    app.certificate.select_certificate(certificate)
-    app.certificate.make_declined_payment(certificate)
-    app.certificate.make_successful_payment(certificate)
-    app.certificate.verify_created_certificate(certificate)
+    call_center.certificate.select_certificate(certificate)
+    call_center.certificate.make_declined_payment(certificate)
+    call_center.certificate.make_successful_payment(certificate)
+    call_center.certificate.verify_created_certificate(certificate)
