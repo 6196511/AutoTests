26f5797bb0dc565a052ba4e836edbd2c9dc09417
Viktor Yenjchak <viktor8623@mail.ru>
2018-08-22 19:40:37 +0300

    Added test for booking with custom price and private party. Added test to verify event manifest and customer event charges page.

--- test/test_GoDo_admin_booking.py
+++ test/test_GoDo_admin_booking.py
@@ -2,7 +2,7 @@ import pytest
 from data.orders import *
 
 
-@pytest.mark.parametrize("tickets", admin_data, ids=[repr(x) for x in admin_data])
+@pytest.mark.parametrize("tickets", admin_data[:18], ids=[repr(x) for x in admin_data[:18]])
 def test_admin_booking(app, tickets):
     """Booking tickets via admin."""
     app.booking.refresh_page()
@@ -100,3 +100,45 @@ def test_admin_booking_with_gift_certificate_and_promo_code(app, order):
     app.booking.refresh_page()
     app.certificate.navigate_to()
     app.certificate.verify_remain_amount(order)
+
+
+@pytest.mark.parametrize("tickets", admin_data[18:20], ids=[repr(x) for x in admin_data[18:20]])
+def test_admin_booking_customer_price(app, tickets):
+    """Booking tickets via admin with custom price."""
+    app.booking.refresh_page()
+    app.booking.select_event(tickets)
+    app.booking.apply_custom_price(tickets)
+    app.booking.fill_out_customer_info(tickets)
+    app.booking.select_payment_method(tickets)
+    app.booking.verify_payment_table(tickets)
+    app.booking.submit_successful_booking()
+
+
+@pytest.mark.parametrize("order", admin_data[22:23], ids=[repr(x) for x in admin_data[22:23]])
+def test_361_private_party(app, order):
+    """Booking tickets for Private Party."""
+    app.booking.refresh_page()
+    app.booking.select_event(order)
+    app.booking.fill_out_customer_info(order)
+    app.booking.select_payment_method(order)
+    app.booking.verify_payment_table(order)
+    app.booking.submit_successful_booking()
+    app.calendar.select_event(order)
+    app.calendar.verify_event_manifest(order)
+    app.calendar.verify_event_status(status="Pending")
+
+
+data = admin_data[:18] + admin_declines + admin_valid_codes[0:8] + admin_invalid_codes[0:5] + admin_invalid_codes[7:] +\
+       admin_booking_with_certificates + admin_cert_discount + admin_data[18:20] + admin_data[22:23]
+
+
+@pytest.mark.parametrize("order", data, ids=[repr(x) for x in data])
+def test_event_manifest_verification(app, order):
+    """Checking booked tickets in the event manifest and customer event page."""
+
+    # GoDo-285 Rounding issue.
+    # GoDo-292, 298, 295, 457, 458 - $0.00
+
+    app.calendar.select_event(order)
+    app.calendar.verify_event_manifest(order)
+    app.calendar.verify_customer_event_admin(order)
