4711179d89298629bc07d56cf49c3c499b0bda35
6196511 <39330561+6196511@users.noreply.github.com>
2018-09-14 14:11:44 +0300

    Added url, your_customer_button

--- pages/people_hub_page.py
+++ pages/people_hub_page.py
@@ -3,8 +3,9 @@ from webium import BasePage, Find
 
 
 class PeopleHubPage(BasePage):
-
+    url = 'https://dev.godo.io/user_hub.aspx'
     add_guide_button = Find(by=By.XPATH, value="//h2[text()='Add Guide']")
+    your_customer_button = Find(by=By.XPATH, value="//h2[text()='Your Customers']")
 
     search_input = Find(by=By.XPATH, value="//input[@placeholder='Start typing to search']")
     name = Find(by=By.XPATH, value="//div[@ng-controller='GuideCompanyExController as vm']//tr[1]/td[1]")
