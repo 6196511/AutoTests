05bf49ca2e59b05ee8b247df9713147ca76c6c60
6196511 <39330561+6196511@users.noreply.github.com>
2018-10-02 10:10:25 +0200

    Update invoice.py

--- pages/invoice.py
+++ pages/invoice.py
@@ -3,6 +3,10 @@ from selenium.webdriver.common.by import By
 
 
 
-class InvoicePage(BasePage):
+class InvoicePageV2(BasePage):
 
     url = 'https://dev.godo.io/invoice_v2.aspx'
+
+class InvoicePage(BasePage):
+
+    url = 'https://dev.godo.io/invoice.aspx'
