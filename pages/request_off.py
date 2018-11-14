326c402d4ebe967a726eb4c0b771e312b01123ac
6196511 <39330561+6196511@users.noreply.github.com>
2018-11-14 17:49:23 +0200

    Add files via upload

---
+++ pages/request_off.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+from webium import BasePage, Find, Finds
+
+
+class RequestOFFPage(BasePage):
+
+    url='https://ci004.godo.io/guide_requestOff.aspx'
+    request_off_entry = Finds(by=By.XPATH, value="//tr[@class='row']|//tr[@class='odd']|//tr[@class='even']")
+    approval_dropdown = Finds(by=By.XPATH, value="//select[contains(@id, 'requestoff_status_')]")
+    not_approved_button = Find(by=By.XPATH, value="//label[@class ='btn btn-danger']")
+    approved_button = Find(by=By.XPATH, value="//label[@class ='btn btn-success']")
+    tables = Finds(by=By.XPATH, value="//table[@id = 'dtRequestOff']")
