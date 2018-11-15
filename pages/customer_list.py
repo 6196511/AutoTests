256437d1601cf094b801613c8da0d422810abf65
6196511 <39330561+6196511@users.noreply.github.com>
2018-11-15 17:56:29 +0200

    Update customer_list.py

--- pages/customer_list.py
+++ pages/customer_list.py
@@ -3,7 +3,7 @@ from webium import BasePage, Find
 
 
 class CustomerListPage(BasePage):
-    url = 'https://dev.godo.io/customer_list.aspx'
+    url = 'https://ci004.godo.io/customer_list.aspx'
     add_customer_button = Find(by=By.XPATH, value="//a[@ng-click='vm.openAddCustomerModal()']")
     country_list = Find(by=By.XPATH, value="//select[@id='customer_country']")
     state_list = Find(by=By.XPATH, value="//select[@id='customer_state']")
