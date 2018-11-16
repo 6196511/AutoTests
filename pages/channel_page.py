421d9286af8bc59250c019e1b3f46411f7c92a6d
6196511 <39330561+6196511@users.noreply.github.com>
2018-11-16 12:26:15 +0200

    Update channel_page.py

--- pages/channel_page.py
+++ pages/channel_page.py
@@ -34,3 +34,4 @@ class ChannelPage(BasePage):
     table_channel_phonenumber = Find(by=By.XPATH, value="//*[@id='dtChannel']/tbody/tr/td[3]")
     table_channel_email = Find(by=By.XPATH, value="//*[@id='dtChannel']/tbody/tr[1]/td[4]")
     table_channel_editbutton = Find(by=By.XPATH, value="//i[@class='fa fa-edit']")
+    table_channel_delete_button = Find(by=By.XPATH, value="//i[@class='fa fa-remove']")
